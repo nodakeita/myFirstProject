6af794ee8f8091239a449366d2f7041d69d5f814
kt_nd <2160625h@stu.kobe-u.ac.jp>
2022-10-19 14:49:33 +0900

    Keita -> What is your name?

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    name = input("What is your name? ")
     print_hi('Keita')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
